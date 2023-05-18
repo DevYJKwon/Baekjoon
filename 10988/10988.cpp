ad79ae9e74698da2925196d12647e0921d3c3c61
DevYJKwon <DevYJKwon>
2023-05-18 19:54:10 +0900

    ii

---
+++ 10988/10988.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    string origin, reversed;
+
+    cin >> origin;
+
+    reversed = origin;
+    reverse(reversed.begin(), reversed.end());
+
+    if (reversed.compare(origin) == 0) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+
+    return 0;
+}
+
---
+++ 1159/1159.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int count[26] = { 0, };
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string temp;
+        cin >> temp;
+        count[temp[0] - 'a']++;
+    }
+    string res="";
+    for (int i = 0; i < 26; i++) {
+        if (count[i] >= 5) {
+            res += 'a' + i;
+        }
+    }
+
+    if (res.size() == 0) {
+        cout << "PREDAJA";
+    }
+    else {
+        cout << res;
+    }
+        return 0;
+}
+

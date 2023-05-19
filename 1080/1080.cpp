a377e17b065f0386d85cb71545933b8232e1a5df
DevYJKwon <DevYJKwon>
2023-05-19 18:13:13 +0900

    update

---
+++ 1080/1080.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int mat1[50][50] = { 0, }, mat2[50][50] = { 0, };
+    int row, col;
+    cin >> row >> col;
+
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%1d", &mat1[i][j]);
+        }
+    }
+     
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            scanf("%1d", &mat2[i][j]);
+        }
+    }
+
+    int count = 0;
+    bool flag = false;
+    for (int i = 0; i < row-2; i++) {
+        for (int j = 0; j < col-2; j++) {
+            if (mat1[i][j] != mat2[i][j]) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        mat1[x][y] = 1 - mat1[x][y];
+                    }
+                }
+                count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (mat1[i][j] != mat2[i][j]) {
+                flag = true;
+                count = -1;
+                break;
+            }
+        }
+        if (flag) {
+            break;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
+
---
+++ 2156/2156.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int dp[100001];
+    int table[10001];
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        cin >> table[i];
+    }
+
+    dp[1] = table[1];
+    dp[2] = table[1] + table[2];
+
+    for (int i = 3; i <= num; i++) {
+        dp[i] = max(dp[i - 1], max(dp[i - 2] + table[i], dp[i - 3] + table[i] + table[i - 1]));
+    }
+
+    cout << dp[num];
+
+    return 0;
+}

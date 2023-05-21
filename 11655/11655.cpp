e0f247df176022b2975f06da74f92b51c6a50970
DevYJKwon <DevYJKwon>
2023-05-21 13:06:29 +0900

    update

---
+++ 11655/11655.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input;
+    getline(cin, input);
+
+    for (int i = 0; i < input.size(); i++) {
+        if (isalpha(input[i])) {
+            if (isupper(input[i])) {
+                input[i] += 13;
+                if (input[i] > 'Z') {
+                    input[i] -= 26;
+                }
+            }
+            else {
+                if (input[i]+13 > 'z') {
+                    input[i] -= 13;
+                }
+                else {
+                    input[i] += 13;
+                }
+            }
+        }
+    }
+
+    cout << input;
+
+    return  0;
+}

4b706ecef220469c4b828d931cbc7c6ffee7769f
DevYJKwon <DevYJKwon>
2023-05-24 18:08:28 +0900

    update

---
+++ 1929/1929.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int start, end;
+    cin >> start >> end;
+
+    for (int i = start; i <= end; i++) {
+        bool isPrime = true;
+        if (i == 1) {
+            isPrime = false;
+        }
+        for (int x = 2; x <= sqrt(i); x++) {
+            if (i % x == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << i << '\n';
+        }
+    }
+}

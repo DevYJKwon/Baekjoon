593f9589b9ed49dc3b1f72f389c4c37cd2ff0571
DevYJKwon <DevYJKwon>
2023-05-26 17:17:26 +0900

    update

---
+++ 1874/1874.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+int num[1000001] = { 0, };
+int main()
+{
+	int n;
+	cin >> n;
+	string res;
+	stack <int> st;
+	for (int i = 1; i <= n; i++) {
+		cin >> num[i];
+	}
+	int index=1;
+	for (int i = 1; i <= n; i++) {
+		st.push(i);
+		res.append("+");
+		while (!st.empty() && st.top() == num[index]) {
+			st.pop();
+			index++;
+			res.append("-");
+		}
+	}
+	if (st.empty()) {
+		for (int i = 0; i < res.size(); i++) {
+			cout << res[i] << '\n';
+		}
+	}
+	else {
+		cout << "NO\n";
+	}
+	return 0;
+}
